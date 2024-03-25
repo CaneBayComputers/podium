87d33452761dbffe05b9de52b41298991d114831
Shawn Welch <shrimpwagon@gmail.com>
2024-03-25 18:21:58 -0400

    Docker install update

--- install.sh
+++ install.sh
@@ -149,19 +149,24 @@ echo-cyan 'Installing Docker ...'
 
 echo-white
 
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 # Add Docker's official GPG key:
 sudo apt-get update
+
 sudo apt-get install ca-certificates curl
+
 sudo install -m 0755 -d /etc/apt/keyrings
+
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
 
 
@@ -193,6 +198,9 @@ sudo apt-get -y -q install \
 	docker-ce \
 	docker-ce-cli \
 	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin \
+	containerd.io \
 	xclip \
 	pv \
 	meld \
@@ -203,7 +211,6 @@ sudo apt-get -y -q install \
   php-bcmath \
   php-cli \
   php-common \
-  php-fpm \
   php-gd \
   php-mongodb \
   php-opcache \
@@ -214,13 +221,6 @@ sudo apt-get -y -q install \
   php-soap \
   php-zip \
   php-xml
-
-
-if ! sudo docker-compose --version > /dev/null 2>&1; then
-
-	sudo pip install docker-compose
-
-fi
 
 echo
 
