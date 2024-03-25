80997924a8d8ba046b30eac17e495f460e51aa80
Shawn Welch <shrimpwagon@gmail.com>
2024-03-25 18:09:25 -0400

    Docker install update

--- install.sh
+++ install.sh
@@ -140,26 +140,28 @@ done
 
 
 ###############################
-# Init apt update
+# Docker
 ###############################
 
 echo
 
-echo-cyan 'Updating and upgrading package repos ...'
+echo-cyan 'Installing Docker ...'
 
 echo-white
 
-if [ ! -f /etc/apt/sources.list.d/docker.list ]; then
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	
-	echo \
-	  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-	  https://download.docker.com/linux/ubuntu \
-	  $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-fi
-
-sudo apt-get update -y -q
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 
 
@@ -172,6 +174,8 @@ echo
 echo-cyan 'Installing packages ...'
 
 echo-white
+
+sudo apt-get update -y -q
 
 sudo apt-get -y -q install \
 	figlet \
