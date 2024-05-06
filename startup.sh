e36a6db929665ef22391867451a4e5dd3b4e2162
Shawn Welch <shrimpwagon@gmail.com>
2024-05-06 16:34:33 -0400

    Startup update

--- startup.sh
+++ startup.sh
@@ -4,56 +4,56 @@ set -e
 
 shopt -s expand_aliases
 
-source ~/.bash_aliases
+cd ~/repos/cbc-development-setup
 
-repos
-
-REPOS=( cbc-development-setup certbot-bash-wrapper cbc-docker-stack cbc-docker-php7-nginx cbc-docker-php8-nginx cbc-laravel-php7 cbc-laravel-php8 )
-
-for REPO in "${REPOS[@]}"
-
-do
-
-	printf "\n------- $REPO\n"
-
-	cd $REPO
-
-	gpull
-
-	cd ..
-
-done
-
-echo
-
-repos
-
-cd cbc-development-setup
+git pull
 
 if ! [ -f is_installed ]; then
 
   source ./install.sh
 
+else
+
+	source ~/.bash_aliases
+
+	repos
+
+	REPOS=( certbot-bash-wrapper cbc-docker-stack cbc-docker-php7-nginx cbc-docker-php8-nginx cbc-laravel-php7 cbc-laravel-php8 )
+
+	for REPO in "${REPOS[@]}"
+
+	do
+
+		printf "\n------- $REPO\n"
+
+		cd $REPO
+
+		gpull
+
+		cd ..
+
+	done
+
+	upcbcstack
+
+	repos
+
+	cd cbc-laravel-php7
+
+	dockerup
+
+	cd ..
+
+	echo
+
+	cd cbc-laravel-php8
+
+	dockerup
+
+	cd ..
+
 fi
 
-upcbcstack
-
-repos
-
-cd cbc-laravel-php7
-
-dockerup
-
-cd ..
-
-echo
-
-cd cbc-laravel-php8
-
-dockerup
-
-cd ..
-
-echo
+echo; echo
 
 read -n 1 -r -s -p $'Press enter to continue...\n'
